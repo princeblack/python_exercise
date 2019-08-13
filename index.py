c459e792e111768a77564d24079b1467f32c5666
princeblack <mahamadicongo@hotmail.com>
2019-08-13 23:13:56 +0200

    python if else

---
+++ index.py
@@ -0,0 +1,10 @@
+a = 5
+if a >= 2:
+    if a <= 8:
+        print("a est dans l'intervalle".)
+    else:
+        print("a n'est pas dans l'intervalle".)
+else:
+    print("a n'est pas dans l'intervalle".)
+    
+    
